be8a53ab6a845ccf08a66aa4cb0748faade10a63
ksegovia81 <segovia.katherine@gmail.com>
2024-10-09 21:42:40 -0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
